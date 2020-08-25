5b8154ad2de188222df7af26f0c799c42511a47e
Michael Lauritsen <i30288741@gmail.com>
2020-08-25 07:08:07 +0200

    Done

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -2,13 +2,13 @@ const path = require('path');
 const CleanPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/app.js',
   output: {
-    filename: 'app.js',
+    filename: '[contenthash].js',
     path: path.resolve(__dirname, 'assets', 'scripts'),
     publicPath: 'assets/scripts/',
   },
-  devtool: 'cheap-module-eval-source-map',
+  devtool: 'cheap-source-map',
   plugins: [new CleanPlugin.CleanWebpackPlugin()],
 };
